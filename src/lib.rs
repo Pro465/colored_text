b69f7f2199c8c4c963e97a4a1d6387976cd309b5
Pro465 <namitapro0987@gmail.com>
2021-09-15 15:13:15 +0530

    make it more concise

--- src/lib.rs
+++ src/lib.rs
@@ -1,42 +1,27 @@
-pub type ColorType = (u8,u8,u8);
+pub type ColorType = (u8, u8, u8);
 
 #[derive(Clone, Copy, Debug)]
 pub struct Color {
-	pub foreground: ColorType,
-	pub background: ColorType
+    pub foreground: ColorType,
+    pub background: ColorType,
 }
 
 impl Color {
-	pub fn set(&self) {
-		use std::io::{ Write , stdout };
-		let mut out = stdout();
+    pub fn set(&self) {
+        use std::io::{stdout, Write};
 
-		out.write_all(
+        print!(
+            "\x1b[38;2;{};{};{}m\x1b[48;2;{};{};{}m",
+            self.foreground.0,
+            self.foreground.1,
+            self.foreground.2,
+            self.background.0,
+            self.background.1,
+            self.background.2,
+        );
 
-		&(0x1b..=0x1b)
-			.chain(format!(
-			"[38;2;{};{};{}m",
-			self.foreground.0,
-			self.foreground.1,
-			self.foreground.2				
-			).bytes())
-			
-			.chain(0x1b..=0x1b)
-			
-			.chain(format!(
-			"[48;2;{};{};{}m",
-			self.background.0,
-			self.background.1,
-			self.background.2,
-			).bytes())
-			.collect::<Vec<u8>>()
-
-		)
-		   .expect("could not set color");
-		
-		out.flush()
-		   .unwrap();
-	}
+        stdout().flush().unwrap();
+    }
 }
 
 pub mod colors;
